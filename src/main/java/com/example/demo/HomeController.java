e340837c650703e0f9a3f238544d7cacaf89481a
kellel14 <kellel14@gmail.com>
2019-07-26 10:27:38 -0400

    edited homecontroller

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,14 +3,47 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
 public class HomeController {
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegistrationPage(@Valid @ModelAttribute("user") User user, BindingResult result,
+                                       Model model) {
+        model.addAttribute("user", user);
+        if(result.hasErrors()){
+            return "registration";
+        }
+        else{
+            userService.saveUser(user);
+            model.addAttribute("message", "User Account Created");
+        }
+        return "index";
+    }
+
+    @PostMapping(value = "/register", method = RequestMethod.POST)
+    public String processRegistrationPage(@Valid @ModelAttribute("user") User user,
+                                          BindingResult result, Model model){
+        model.addAttribute("user", user);
+        if (result.hasErrors()){
+            return "registration";
+        }
+        else{
+            userService.saveUser(user);
+            model.addAttribute("message", "User Account Created");
+        }
+        return "index";
+    }
 
     @RequestMapping("/")
     public String index(){
